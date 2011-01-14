f61c0dd9e130eeece11d86dd48e55d717ce4dcd6
Allan Caffee <allanc@aweber.com>
2011-01-14 14:57:42 -0500

    Improved acceptance test

--- httpqueue/tests/test_acceptance.py
+++ httpqueue/tests/test_acceptance.py
@@ -5,21 +5,34 @@ These tests verify the basic functionality of the system as a whole.
 
 import datetime
 
+import mongokit
+
 import httpqueue.app
-from base_flask_test import BaseViewTest
+from base_flask_test import BaseViewTest, TestingConfig
 
 
 class TestAcceptance(BaseViewTest):
     "Test the app end to end."
 
-    def test_push_item(self):
-        data = '{\n  "do": "something"\n}'
-        priority = datetime.datetime.utcnow().isoformat()
+    def setUp(self):
+        BaseViewTest.setUp(self)
+        self.epoch = datetime.datetime(1970, 1, 1, 0, 0, 0, 1).isoformat()
+        self.nowish = datetime.datetime.utcnow().isoformat()
+
+        conn = mongokit.Connection(TestingConfig.MONGODB_HOST,
+                                   TestingConfig.MONGODB_PORT)
+        conn.test.drop_collection('pq_foo')
+
+    def _post_data(self, priority, data):
         resp = self.client.post('/queue/foo/',
                                 headers=[('X-httPQueue-Priority', priority)],
                                 data=data, content_type='application/json')
 
         self.assertEqual(resp.status_code, 200)
+
+    def test_push_item(self):
+        data = '{\n  "do": "something"\n}'
+        self._post_data(self.nowish, data)
 
         resp = self.client.get('/queue/foo/')
         self.assertEqual(resp.status_code, 200)
@@ -37,3 +50,12 @@ class TestAcceptance(BaseViewTest):
         resp = self.client.get('/queue/foo/')
 
         self.assertEqual(resp.status_code, 204)
+
+    def test_returns_in_order(self):
+        data_first = '{\n  "do": "first"\n}'
+        data_second = '{\n  "do": "second"\n}'
+
+        self._post_data(self.nowish, data_second)
+        self._post_data(self.epoch, data_first)
+
+        resp = self.client.get('/queue/foo/')
